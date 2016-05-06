93d0b89b069030ba838d14450491fda17812666a
inandu1516 <inandu1516@gmail.com>
2016-05-06 12:24:27 +0200

    Mongoose Schema Find Array Collection

--- server.js
+++ server.js
@@ -11,26 +11,39 @@ var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 
 db.once('open', function() {
-    console.log("We are connected!");
+    console.log("Connected...OK");
     /**
-     * Mongoose will validate the data inserted against this Schema specifications
-     * if mistype name of field or wrong data type its gonna complain
+     * Schema Specification
+     * defines data types of the particular collection
      */
     var FormSchema = new mongoose.Schema({
         name: String,
-        created: Date
-    });
+        created: {type: Date, default: Date.now}
+    }, {collection:"form"});
 
     /**
      * Data Model Collection
-     * Mongoose choose the plural name of the data model to create the collection
+     * the object to make CRUDs for this collection
      */
     var Form = mongoose.model("Form", FormSchema);
 
-    var form1 = new Form({name: "Form 1", created: new Date});
-    var form2 = new Form({name: "Form 2", created: new Date});
+    var form1 = new Form({name: "Form 1"});
+    var form2 = new Form({name: "Form 2"});
 
-    /** save() insert date (it has to match the schema defined) */
+    /** insert date */
     form1.save();
     form2.save();
+
+    /**
+     * Asynchronous operation, program doesn't stop,
+     * it waits for the callback with the data asked for.
+     * When data comes, it displays the callback function passed.
+     */
+    Form.find(receiveData);
+
+    function receiveData(err, data) {
+        console.log(err);       //displays null if no error
+        console.log(data);     //displays the array collection
+    }
+
 });
