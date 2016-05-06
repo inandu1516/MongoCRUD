f0c731b6c8892367fe5257035b7d6e6afda06bba
inandu1516 <inandu1516@gmail.com>
2016-05-06 13:22:18 +0200

    Express Server

--- ExpressServer.js
+++ ExpressServer.js
@@ -6,11 +6,37 @@
  * node ExpressServer.js
  */
 
+//Express
 var express = require('express');
 var app = express();
 
+//Mongoose
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/CRUD');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+
+db.once('open', function() {
+    console.log("Connected...OK");
+    var FormSchema = new mongoose.Schema({
+        name: String,
+        created: {type: Date, default: Date.now}
+    }, {collection:"form"});
+    Form = mongoose.model("Form", FormSchema);
+});
+
+//Routing
 app.get('/', function (req, res) {
     res.send("hello world");
+    console.log("Connected.....port 3000");
+});
+
+app.get('/find', function (req, res) {
+    res.send("finding data...");
+    console.log("directing to .../find");
+    Form.findById("572c7394202dce0c1c193ae2", function (err, data) {
+        console.log("\nData finded:\n" + data);
+    });
 });
 
 app.listen(3000);
